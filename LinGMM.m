39355c667f0b871b59120988d1531a25aa3a5f0a
nzy129 <nzy129@163.com>
2022-09-06 12:04:23 -0400

    Update LinGMM.m

--- LinGMM.m
+++ LinGMM.m
@@ -1,6 +1,9 @@
+% function for linear GMM, input is data Y, X,and IV Z, weighting matrix W.
+% Written by Zheyu Ni at the Ohio State University
+% Output is GMM coefficient beta, VCV matrix and predicted errors.
+
 function [beta, VCV, error]=LinGMM(Y,X,Z,W)
-%%function for linear GMM, input is data Y, X,and IV Z, weighting matrix W.
-%%Output is GMM coefficient beta, VCV matrix and predicted errors.
+
 N=size(Y,1);
 beta=(X'*Z*W*Z'*X)\(X'*Z*W*Z'*Y);
 error=Y-X*beta;
@@ -9,12 +12,6 @@ Omega=1/N*(M'*M);
 G=1/N*Z'*X;
 
 VCV=(G'*W*G)\(G'*W*Omega*W*G)/(G'*W*G);
-
-
-
-
-
-
 
 
 end
